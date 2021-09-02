58809e4bba58aa00987de641c3ee5d7668e0e40b
Gašper Petelin <gasper.petelin@ijs.si>
2021-09-02 14:27:19 +0200

    Perform SOM clustering

--- SOM_grid_search.R
+++ SOM_grid_search.R
@@ -1,47 +1,56 @@
+install.packages("aweSOM", repos='http://cran.us.r-project.org')
+# TODO add version
 library(aweSOM)
+set.seed(1465)
+args <- commandArgs(TRUE)
 
-data<-read.table("/Users/tomeeftimov/Nextcloud/NeurIPS/all_data_ts_clean_v2.txt",sep="\t",header=TRUE)
+print("Load data")
+data<-read.table("data/Features/all_data_ts_clean_v2.txt",sep="\t",header=TRUE)
 
+features_file<-args[2]
+data<-read.table(features_file, sep="\t",header=TRUE)
 data_temp<-scale(data.matrix(data))
 
+
 ### RNG Seed (for reproducibility)
-init <- somInit(data_temp, 45, 45)
-som.model <- kohonen::som(data_temp, grid = kohonen::somgrid(45, 45, "hexagonal"), 
-+                          rlen = 100, alpha = c(0.05, 0.01), radius = c(2.65,-2.65), 
-+                          dist.fcts = "sumofsquares", init = init)
+#init <- somInit(data_temp, 45, 45)
+#som.model <- kohonen::som(data_temp, grid = kohonen::somgrid(45, 45, "hexagonal"), rlen = 100, alpha = c(0.05, 0.01), radius = c(2.65,-2.65), dist.fcts = "sumofsquares", init = init)
 
 
-somQuality(som.model,data_temp)
+#somQuality(som.model,data_temp)
 
 
 
-set.seed(1465)
-m<-5
-n<-50
-qunat_err<-c()
-expl_var<-c()
-topp_err<-c()
-kaski_lagus_err<-c()
-cellpop<-list()
-for (i in seq(m,n)){
-    
-        init<-somInit(data_temp,i,i)
-        som.model <- kohonen::som(data_temp, grid = kohonen::somgrid(i, i, "hexagonal"), 
-                         rlen = 100, alpha = c(0.05, 0.01), radius = c(2.65,-2.65), 
-                         dist.fcts = "sumofsquares", init = init)
-        x<-somQuality(som.model, data_temp) 
-        qunat_err[i]<-x$err.quant
-        expl_var[i]<-x$err.varratio
-        topp_err[i]<-x$err.topo
-        kaski_lagus_err[i]<-x$err.kaski
-        cellpop[[i]]<-x$cellpop
-       }
+#set.seed(1465)
+#m<-5
+#n<-50
+#qunat_err<-c()
+#expl_var<-c()
+#topp_err<-c()
+#kaski_lagus_err<-c()
+#cellpop<-list()
+#for (i in seq(m,n)){
+#    
+#        init<-somInit(data_temp,i,i)
+#        som.model <- kohonen::som(data_temp, grid = kohonen::somgrid(i, i, "hexagonal"), 
+#                         rlen = 100, alpha = c(0.05, 0.01), radius = c(2.65,-2.65), 
+#                         dist.fcts = "sumofsquares", init = init)
+#        x<-somQuality(som.model, data_temp) 
+#        qunat_err[i]<-x$err.quant
+#        expl_var[i]<-x$err.varratio
+#        topp_err[i]<-x$err.topo
+#        kaski_lagus_err[i]<-x$err.kaski
+#        cellpop[[i]]<-x$cellpop
+#       }
 
 # Example how to generate the 48x48
-set.seed(1465)
-i<-48
+
+i<-strtoi(args[1])
+print("Train SOM with size")
 init<-somInit(data_temp,i,i)
-        som.model <- kohonen::som(data_temp, grid = kohonen::somgrid(i, i, "hexagonal"), 
-                         rlen = 100, alpha = c(0.05, 0.01), radius = c(2.65,-2.65), 
-                         dist.fcts = "sumofsquares", init = init)
+
+#TODO change iterations to 100
+som.model <- kohonen::som(data_temp, grid = kohonen::somgrid(i, i, "hexagonal"), rlen = 100, alpha = c(0.05, 0.01), radius = c(2.65,-2.65), dist.fcts = "sumofsquares", init = init)
 x<-somQuality(som.model, data_temp) 
+
+write.table(som.model$codes, "/home/gasper/Desktop/neki.csv")
---
+++ coverage_matrix.R
@@ -0,0 +1,84 @@
+args <- commandArgs(TRUE)
+
+files<-list.files("ts_fresh_lvl1_full/Univariate2018_ts/Univariate_ts",full.names=TRUE,recursive=TRUE)
+
+data<-list()
+for (i in 1:length(files)){
+    data[[i]]<-read.ts(files[i],header=TRUE,sep=",")
+}
+
+names<-c()
+for(i in 1:length(files)){
+    names[i]<-strsplit(strsplit(files[i],"Univariate_ts/")[[1]][2],"/")[[1]][1]
+}
+
+count<-c()
+for(i in 1:length(data)){
+    count[i]<-nrow(data[[i]])
+}
+
+df_counts<-cbind(names,count)
+
+cell_inst<-read.table("cell_instance.txt",sep="\t")
+
+
+coverage_matrix <- function(file, output_file) {
+    clusters<-read.csv(file,sep="\t",header=FALSE)
+    clusters<-cbind(clusters,seq(1,35*35,1))
+    colnames(clusters)<-c("groupes","cell")
+    
+    clust<-c()
+    for( i in 1:nrow(cell_inst)){
+        clust[i]<-clusters$groupes[which(clusters$cell==cell_inst$t[i])]
+
+    }
+
+
+    df1<-cbind(cell_inst,clust)
+    colnames(df1)<-c("name","cell","clust")
+    
+    l<-list()
+    counter<-1
+    for(i in sort(unique(clust))){
+        index<-which(df1$clust==i)
+        temp<-df1[index,]
+
+
+        y<-c()
+        for(j in 1:nrow(temp)){
+            y[j]<-strsplit(temp$name[j],"_")[[1]][1]
+        }
+        l[[counter]]<-unique(y)
+        counter<-counter+1
+        
+        coverage<-matrix(0,length(unique(clust)),113)
+        counter<-1
+        for(i in sort(unique(clust))){
+            index<-which(df1$clust==i)
+            temp<-df1[index,]
+            y<-c()
+            for(j in 1:nrow(temp)){
+                y[j]<-strsplit(temp$name[j],"_")[[1]][1]
+            }
+
+            for(k in 1:nrow(df_counts)){
+                coverage[counter,k]<-length(which(y==df_counts[k,1]))/as.numeric(as.character(df_counts[k,2]))*100
+            }
+            counter<-counter+1
+        }
+    }
+    
+    #m<-basename(file)
+    rownames(coverage)<-paste("Cluster", seq(1,length(unique(clust)),1))
+    colnames(coverage)<-names
+
+    #dir<-"coverage_matrix/"
+    #path<-paste(dir, m)
+    #dir.create(dir)
+    write.table(coverage, output_file, sep="\t")
+}
+
+input_file<-args[1]
+output_file<-args[2]
+
+coverage_matrix(input_file, output_file)
